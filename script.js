f58e0a6425f8dac78f7ba3bd40e556452279b80f
TRMSC <solidware@web.de>
2022-05-01 11:50:29 +0200

    Add more files

--- script.js
+++ script.js
@@ -8,18 +8,25 @@ function getYear() {
 
 function exportZip() {
   var zip = new JSZip();
-        
+  
+  // Add files in data/
   let maincss = fetchFile('data/main.css');
   zip.file("main.css", maincss, {binary: true});      
   let indexhtml = fetchFile('data/index.html');
   zip.file("index.html", indexhtml, {binary: true});
 
+  // Add files in data/js/
   let js = zip.folder("js");
   let esmoduleshimsjs = fetchFile('data/js/es-module-shims.js');
   js.file("es-module-shims.js", esmoduleshimsjs, {binary: true});
   let threemodulejs = fetchFile('data/js/three.module.js');
   js.file("three.module.js", threemodulejs, {binary: true});
-        
+  
+  // Add files in data/js/controls
+  let jsControls = zip.folder("js/controls");
+  let orbitcontrolsjs = fetchFile('data/js/controls/OrbitControls.js');
+  jsControls.file("OrbitControls.js", orbitcontrolsjs, {binary: true});
+
   zip.generateAsync({type:"blob"})
   .then(function(content) {
     // see FileSaver.js
