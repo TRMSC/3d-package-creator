88701010eb594ffb625bbced6138db5eab2a92a6
TRMSC <solidware@web.de>
2022-05-01 12:20:06 +0200

    Add remaining files

--- script.js
+++ script.js
@@ -34,6 +34,32 @@ function exportZip() {
   file = fetchFile('data/js/environments/RoomEnvironment.js');
   folder.file("RoomEnvironment.js", file, {binary: true});
 
+  // Add files in data/js/libs
+  folder = zip.folder("js/libs");
+  file = fetchFile('data/js/libs/meshopt_decoder.module.js');
+  folder.file("meshopt_decoder.module.js", file, {binary: true});
+
+  // Add files in data/js/libs/basis
+  folder = zip.folder("js/libs/basis");
+  file = fetchFile('data/js/libs/basis/README.md');
+  folder.file("README.md", file, {binary: true});
+  file = fetchFile('data/js/libs/basis/basis_transcoder.js');
+  folder.file("basis_transcoder.js", file, {binary: true});
+  file = fetchFile('data/js/libs/basis/basis_transcoder.wasm');
+  folder.file("basis_transcoder.wasm", file, {binary: true});
+
+  // Add files in data/js/loaders
+  folder = zip.folder("js/loaders");
+  file = fetchFile('data/js/loaders/GLTFLoader.js');
+  folder.file("GLTFLoader.js", file, {binary: true});
+  file = fetchFile('data/js/loaders/KTX2Loader.js');
+  folder.file("KTX2Loader.js", file, {binary: true});
+
+  // Add files in data/js/utils
+  folder = zip.folder("js/utils");
+  file = fetchFile('data/js/utils/WorkerPool.js');
+  folder.file("WorkerPool.js", file, {binary: true});
+
   // Create zip file
   zip.generateAsync({type:"blob"})
   .then(function(content) {
