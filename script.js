53cef0dae5e19c8804d4282f6a2c12227f5e519e
TRMSC <solidware@web.de>
2022-05-01 12:11:01 +0200

    Change variables

--- script.js
+++ script.js
@@ -8,25 +8,33 @@ function getYear() {
 
 function exportZip() {
   var zip = new JSZip();
+  var file;
+  var folder;
   
   // Add files in data/
-  let maincss = fetchFile('data/main.css');
-  zip.file("main.css", maincss, {binary: true});      
-  let indexhtml = fetchFile('data/index.html');
-  zip.file("index.html", indexhtml, {binary: true});
+  file = fetchFile('data/main.css');
+  zip.file("main.css", file, {binary: true});      
+  file = fetchFile('data/index.html');
+  zip.file("index.html", file, {binary: true});
 
   // Add files in data/js/
-  let js = zip.folder("js");
-  let esmoduleshimsjs = fetchFile('data/js/es-module-shims.js');
-  js.file("es-module-shims.js", esmoduleshimsjs, {binary: true});
-  let threemodulejs = fetchFile('data/js/three.module.js');
-  js.file("three.module.js", threemodulejs, {binary: true});
+  folder = zip.folder("js");
+  file = fetchFile('data/js/es-module-shims.js');
+  folder.file("es-module-shims.js", file, {binary: true});
+  file = fetchFile('data/js/three.module.js');
+  folder.file("three.module.js", file, {binary: true});
   
   // Add files in data/js/controls
-  let jsControls = zip.folder("js/controls");
-  let orbitcontrolsjs = fetchFile('data/js/controls/OrbitControls.js');
-  jsControls.file("OrbitControls.js", orbitcontrolsjs, {binary: true});
+  folder = zip.folder("js/controls");
+  file = fetchFile('data/js/controls/OrbitControls.js');
+  folder.file("OrbitControls.js", file, {binary: true});
 
+  // Add files in data/js/environments
+  folder = zip.folder("js/environments");
+  file = fetchFile('data/js/environments/RoomEnvironment.js');
+  folder.file("RoomEnvironment.js", file, {binary: true});
+
+  // Create zip file
   zip.generateAsync({type:"blob"})
   .then(function(content) {
     // see FileSaver.js
