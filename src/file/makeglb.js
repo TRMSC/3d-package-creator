269a48080a32fc30147d529bcd076c25f5cc464b
TRMSC <solidware@web.de>
2022-05-06 18:32:29 +0200

    Add blob

--- src/file/makeglb.js
+++ src/file/makeglb.js
@@ -188,8 +188,8 @@ function traverseFileTree(item, path) {
             for (let i in zip.files) {
               //console.log(zip.files[i]);
               //entry = zip.files[i];  // CONVERT FROM ZIP OBJECT TO FILE ENTRY
-              var zipblob = new Blob([this]);
-              zipreader.readAsArrayBuffer (zipblob);
+              var blob = new Blob([this]);
+              zipreader.readAsArrayBuffer (blob);
               zipreader.onload = function() {
                 traverseFileTree(reader.result);
                 console.log(reader.result);
