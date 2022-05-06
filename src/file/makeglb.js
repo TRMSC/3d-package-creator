48195f6bfd430284464d9c65e35dc9e24378c1ac
TRMSC <solidware@web.de>
2022-05-06 17:05:54 +0200

    Change entry

--- src/file/makeglb.js
+++ src/file/makeglb.js
@@ -184,8 +184,16 @@ function traverseFileTree(item, path) {
             console.log(items);
 
             for (let i in zip.files) {
-              console.log(zip.files[i]);
-              entry = zip.files[i].webkitGetAsEntry();
+              if (zip.files[i].getAsEntry) {
+                entry = zip.files[i].getAsEntry();
+                console.log (zip.files[i] + " via getAsEntry()");
+              } else if (zip.files[i].webkitGetAsEntry) {
+                entry = zip.files[i].webkitGetAsEntry();
+                console.log (zip.files[i] + " via webkitGetAsEntry()");
+              }
+
+              //console.log(zip.files[i]);
+              //entry = zip.files[i].webkitGetAsEntry();
               console.log(entry);
               traverseFileTree(entry);
             }
