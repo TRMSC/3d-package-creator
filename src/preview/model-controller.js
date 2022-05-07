b4f5405333a8083d66ef5b61005e32d07ae13fc1
TRMSC <solidware@web.de>
2022-05-07 21:15:27 +0200

    Print scale

--- src/preview/model-controller.js
+++ src/preview/model-controller.js
@@ -36,6 +36,8 @@ AFRAME.registerComponent('model-controller', {
 
 
                         var max = Math.max(size.x, size.y, size.z);
+                        modelScale = 2 / max; 
+                        console.log ("scale is " + modelScale)
 
                         if (!isNaN(max) && max !== Infinity && max > 0.1) {
                             this.currScale = 2 / max; // 2 is according the experience, need to be confirmed;
